4b651042b69baaba13f2adcac5ea2e5a08a6bd6f
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2022-04-14 12:22:21 +0300

    hello world front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { say_hello_name } from "./utils/hello_world";
 
 function App() {
+  const [uname, setUname] = useState("");
+  const [h1Text, setHeading] = useState("");
+  const [isMousedOver, setMouseOver] = useState(false);
+  function handle_change(event) {
+    setHeading("");
+    setUname(event.target.value);
+    console.log(event.target.value);
+  }
+
+  function handleMouseOver() {
+    setMouseOver(!isMousedOver);
+  }
+
+  async function handle_click(event) {
+    event.preventDefault();
+    const result = await say_hello_name(uname);
+    console.log({ result });
+    setHeading(result);
+    setUname("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1> {h1Text} </h1>
+      <form onSubmit={handle_click}>
+        <input
+          onChange={handle_change}
+          type="text"
+          placeholder="What's your name?"
+          value={uname}
+        />
+        <button
+          style={{ backgroundColor: isMousedOver ? "Yellow" : "white" }}
+          type="submit"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOver}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
